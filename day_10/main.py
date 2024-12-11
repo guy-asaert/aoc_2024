0a47e7d928c6197dfe646b923bd60a0d96c81de1
Guy Asaert <gasaert@gmail.com>
2024-12-11 06:22:57 +0000

    Start day 10

--- day_10/main.py
+++ day_10/main.py
@@ -1,16 +1,25 @@
 from utils import read_lines
 
-SAMPLE_INPUT = """"""
+SAMPLE_INPUT = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
 
 
 
 def solve_part1():
 
-    # for row, report_line in enumerate(SAMPLE_INPUT.split("\n")):
-    for row, report_line in enumerate(read_lines(__file__)):
-        print(report_line)
-        pass
-
+    map = []
+    for row, report_line in enumerate(SAMPLE_INPUT.split("\n")):
+    # for row, report_line in enumerate(read_lines(__file__)):
+        row = [int(c) for c in report_line]
+        map.append(row)
+    
+    pass
 
 if __name__ == "__main__":
     solve_part1()
