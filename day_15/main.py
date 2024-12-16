b2841484b1b65bc0b7ae3a1262a102ee85c24231
Guy Asaert <gasaert@gmail.com>
2024-12-16 05:00:45 +0000

    Day 15 done

--- day_15/main.py
+++ day_15/main.py
@@ -1,6 +1,7 @@
 from utils import read_lines
 from dataclasses import dataclass
 from collections import namedtuple
+import copy
 
 SAMPLE_INPUT = """#######
 #...#.#
@@ -24,6 +25,10 @@ class Positionable:
     def __repr__(self):
         return f"({self.x}, {self.y})"
 
+@dataclass
+class Tracker:
+    boxes_moved: int = 0
+
 
 def print_warehouse(robot, walls, boxes):
     width = max([x.x for x in walls]) + 1
@@ -43,9 +48,11 @@ def print_warehouse(robot, walls, boxes):
     print()
 
 
-def print_double_width_warehouse(robot, walls, boxes):
+def print_double_width_warehouse(robot, walls, boxes, file_handle=None):
     width = max([x.x for x in walls]) + 1
     height = max([x.y for x in walls]) + 1
+
+    ware_house_state = []
     for y in range(height):
         x = 0
         row_string = ""
@@ -66,10 +73,14 @@ def print_double_width_warehouse(robot, walls, boxes):
                 row_string += "."
                 # print(".", end="")
                 x += 1
-        print(row_string)
-        # print()
+        ware_house_state.append(row_string)
 
-    print()
+    if file_handle:
+        file_handle.write("\n".join(ware_house_state))
+        file_handle.write("\n")
+    else:
+        print("\n".join(ware_house_state))
+        print()
 
 def move_object(object, move):
     if move == "^":
@@ -134,7 +145,7 @@ def solve_part1():
     print(sum_of_gps_coordinates)
         
 
-def process_moving_thing_wide(x, y, direction, walls, boxes, last_collision_box_index=-1):
+def process_moving_thing_wide(x, y, direction, walls, boxes, tracker, box_movements, last_collision_box_index=-1):
 
     for wall in walls:
         if (last_collision_box_index == -1 and wall.y == y and wall.x <= x <= wall.x + 1) or \
@@ -142,41 +153,48 @@ def process_moving_thing_wide(x, y, direction, walls, boxes, last_collision_box_
             return False
 
     can_move = True
-    movements = []
+    boxes_moved = 0
     for box_index, box in enumerate(boxes):
         if box_index == last_collision_box_index:
             continue
         if (last_collision_box_index == -1 and box.y == y and box.x <= x <= box.x + 1) or \
            (last_collision_box_index != -1 and box.y == y and abs(box.x -x) < 2):
-            # collide with box        
+            # collide with box
+            boxes_moved += 1
             move_x, move_y = move_object(box, direction)
-            if process_moving_thing_wide(move_x, move_y, direction, walls, boxes, box_index):
-                movements.append((box_index, move_x, move_y)) 
+            if process_moving_thing_wide(move_x, move_y, direction, walls, boxes, tracker, box_movements, box_index):
+                box_movements.add((box_index, move_x, move_y)) 
             else:
                 can_move = False
 
-    if can_move:
-        for box_index, move_x, move_y in movements:
-            boxes[box_index].x = move_x
-            boxes[box_index].y = move_y
+
 
     return can_move # no obstacles
 
-SAMPLE_INPUT_WIDE = """#######
-#...#.#
-#.....#
-#..OO@#
-#..O..#
-#.....#
-#######
+SAMPLE_INPUT_WIDE = """##########
+#........#
+#........#
+#.....#..#
+#...OOO..#
+#...OO...#
+#..@O....#
+#........#
+#........#
+##########
 
-<vv<<^^<<^^"""
+>>><<^><vv>>>^"""
+
+
+def result(boxes):
+    gps_coords = sum([box.x + 100 * box.y for box in boxes])
+    return gps_coords
 
 def solve_part2():
     """ Double width walls and boxes """
     robot_moves = ""
     walls = []
     boxes = []
+
     # for row, report_line in enumerate(SAMPLE_INPUT_WIDE.split("\n")):
     for row, report_line in enumerate(read_lines(__file__)):
         if not report_line:
@@ -194,26 +212,24 @@ def solve_part2():
 
     print_double_width_warehouse(robot, walls, boxes)
 
-    for move_index, move in enumerate(robot_moves):
-        x, y = move_object(robot, move)
-        # if move_index == 94:
-        #     print("check this")
+    max_boxes_moved = 0
+    print(f'We have {len(boxes)} boxes')
+    with open("day_15/warehouse.txt", "w") as f:
+        for move_index, move in enumerate(robot_moves):
+            x, y = move_object(robot, move)
+            tracker = Tracker()
+            box_movements = set()
+            if process_moving_thing_wide(x, y, move, walls, boxes, tracker, box_movements):
+                robot = Positionable(x, y)
+                for box_index, move_x, move_y in box_movements:
+                    boxes[box_index].x = move_x
+                    boxes[box_index].y = move_y
 
-        if process_moving_thing_wide(x, y, move, walls, boxes):
-            robot = Positionable(x, y)
-        
-        # print((f'Move: ({move_index}) {move}'))
-        # print_double_width_warehouse(robot, walls, boxes)
-        # pass
+            # print(f'Completed {move_index + 1} moves')
+            # print_double_width_warehouse(robot, walls, boxes)
 
-    print(f'Completed {move_index + 1} moves')
-    print_double_width_warehouse(robot, walls, boxes)
-
-    sum_of_gps_coordinates = 0
-    for box in boxes:
-        sum_of_gps_coordinates += box.x + 100 * box.y
-
-    print(sum_of_gps_coordinates)    
+        # print(f'Max boxes moved: {max_boxes_moved}')
+        print(f'Result {result(boxes)}.')
 
 if __name__ == "__main__":
     solve_part2()
