5dcc761240a3a4db4930e85498875936567dadd0
Guy Asaert <gasaert@gmail.com>
2025-02-24 22:09:09 +0000

    Completed day 21

--- day_21/main_v3.py
+++ day_21/main_v3.py
@@ -95,8 +95,8 @@ def move_from_to_next_pad_sequences(from_key, to_key, pad_identifier, level_numb
     tap_sequences = [taps for taps in set([x_then_y, y_then_x]) if not taps.startswith(bad_pattern)]
 
     if level_number == max_level or len(tap_sequences[0]) < 2:
-        cache[(from_key, to_key, level_number)] = tap_sequences[0]
-        return tap_sequences[0]
+        cache[(from_key, to_key, level_number)] = len(tap_sequences[0])
+        return len(tap_sequences[0])
     
     if len(tap_sequences) > 1:
         assert len(tap_sequences[0]) == len(tap_sequences[1])
@@ -104,7 +104,7 @@ def move_from_to_next_pad_sequences(from_key, to_key, pad_identifier, level_numb
     expanded_tap_sequence = []
 
     for tap_sequence in tap_sequences:
-        this_expanded_tap_sequence = ""
+        this_expanded_tap_sequence = 0
         next_from_key = 'A'
         for next_to_key in tap_sequence:
             next_level_tap_sequences = move_from_to_next_pad_sequences(
@@ -115,31 +115,13 @@ def move_from_to_next_pad_sequences(from_key, to_key, pad_identifier, level_numb
         expanded_tap_sequence.append(this_expanded_tap_sequence)
 
     if len(expanded_tap_sequence) > 1:
-        cache[(from_key, to_key, level_number)] = min(expanded_tap_sequence, key=lambda x: len(x))
-        return min(expanded_tap_sequence, key=lambda x: len(x))
+        ret = min(expanded_tap_sequence) 
+        cache[(from_key, to_key, level_number)] = ret
     else:
-        cache[(from_key, to_key, level_number)] = expanded_tap_sequence[0]
-        return expanded_tap_sequence[0]
+        ret = expanded_tap_sequence[0]
+        cache[(from_key, to_key, level_number)] = ret
 
-
-# def generate_optimised_move_patrix(pad_identifier):
-
-#     pad = KEY_PADS[pad_identifier].pad
-
-#     # generate all possible movements
-#     movements = set(combinations_with_replacement(pad.keys(), 2))
-#     other_way_around = list()
-#     for movement in movements:
-#         other_way_around.append((movement[1], movement[0]))
-#     movements.update(other_way_around)
-
-#     # movements = [('A', '0')]
-    
-#     for from_key, to_key in movements:
-#         move_options = move_from_to_next_pad_sequences(from_key, to_key, pad_identifier, level_number=0, max_level=2)
-#         print(f"from_key: {from_key}, to_key: {to_key}, move_options: {move_options}")
-
-
+    return ret
 
 
 def main():
@@ -148,13 +130,13 @@ def main():
     for code in PUZZLE_CODES.split("\n"):
         from_key = 'A'
         total_taps = 0
-        my_code = ""
+        # my_code = ""
         for to_key in code:
             taps = move_from_to_next_pad_sequences(
-                from_key, to_key, NUMERIC_PAD_IDENTIFIER, level_number=1, max_level=22)
-            print(f'from_key: {from_key}, to_key: {to_key}, taps: {len(taps)}')
-            total_taps += len(taps)
-            my_code += taps
+                from_key, to_key, NUMERIC_PAD_IDENTIFIER, level_number=1, max_level=26)
+            print(f'from_key: {from_key}, to_key: {to_key}, taps: {taps}')
+            total_taps += taps
+            # my_code += taps
             from_key = to_key
         answer += total_taps * int(code[:3])
         print(f"total_taps for code {code}: {total_taps * int(code[:3])}")
@@ -163,56 +145,4 @@ def main():
     print(f"Answer: {answer}")  
 
 if __name__ == "__main__":
-    profiler = cProfile.Profile()
-    profiler.enable()
     main()
-    profiler.disable()
-    profiler.print_stats(sort='time')
-
-
-# @lru_cache(maxsize=None)
-# def move_from_to(from_key, to_key, key_pad_identifier, last_level=False):
-#     key_pad = KEY_PADS[key_pad_identifier]
-#     # only consider the x then y or y then x moves
-#     from_location = key_pad.pad.get(from_key)
-#     to_location = key_pad.pad.get(to_key)
-#     if not from_location:
-#         raise ValueError(f"Invalid key {from_key}")
-#     if not to_location:
-#         raise ValueError(f"Invalid key {to_key}")
-#     x_offset, y_offset = to_location[0] - from_location[0], to_location[1] - from_location[1]
-#     x_y_direction_taps, y_x_direction_taps = get_taps(x_offset, y_offset)
-
-#     bad_pattern = key_pad.bad_pattern.get(from_key, "xxxxxxxxxxxxxx")
-#     taps_choices = [taps for taps in set([x_y_direction_taps, y_x_direction_taps]) if not taps.startswith(bad_pattern)]
-
-#     if len(taps_choices) > 1:
-#         # we have more than one choice, so we need to see which is the best choice by looking at the next level
-#         print(f"from_key: {from_key}, to_key: {to_key}, taps_choices: {taps_choices}")
-#         if not last_level:
-#             # which is the best tap sequence to use? We need to see what the shortest next level tap sequence is
-#             shortest_tap_sequence = sys.maxsize
-#             chosen_tap_sequence = None
-#             for tap_choice in taps_choices:
-#                 next_level_taps = tap_code('A' + tap_choice, [DIRECTION_PAD_IDENTIFIER])
-#                 if next_level_taps < shortest_tap_sequence:
-#                     shortest_tap_sequence = next_level_taps
-#                     chosen_tap_sequence = tap_choice
-#             print(f"from_key: {from_key}, to_key: {to_key}, chosen_tap_sequence: {chosen_tap_sequence}")
-#             return chosen_tap_sequence
-
-#     if len(taps_choices) > 1:
-#         print(f"from_key: {from_key}, to_key: {to_key}, taps_choices: {taps_choices}")
-#     return taps_choices[0]
-
-
-# def tap_code(code, key_pad_chain):
-#     number_of_taps = 0
-#     for index, from_key in enumerate(code[:-1]):
-#         key_pad_taps = move_from_to(from_key, code[index+1], key_pad_chain[0], len(key_pad_chain) == 1)
-#         if len(key_pad_chain) > 1:
-#             number_of_taps += tap_code('A' + key_pad_taps, key_pad_chain[1:])
-#         else:
-#             number_of_taps += len(key_pad_taps)
-
-#     return number_of_taps
